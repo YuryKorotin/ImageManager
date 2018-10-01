0c74b5e34846a4d905c701dd9f88f38722385025
YuryKorotin <yurykorotin73@gmail.com>
2018-10-01 22:37:25 +0400

    Add specification for format method in test

--- image_search_server_test.go
+++ image_search_server_test.go
@@ -2,10 +2,21 @@ package main
 
 import "testing"
 
-func TestSearchImage(t *testing.T) {
-	var body = "{\"code\":102, \"lang\": \"ru-en\", \"text\": [\"translation\"] }"
-	translation, _ := getTranslations([]byte(body))
-	if (translation.Texts[0] != "translation") {
-       		t.Errorf("Translation was incorrect, got: %s, want: %s.", translation.Texts[0], "translation")
-    	}
+func TestGetTranslations(t *testing.T) {
+  var body = "{\"code\":102, \"lang\": \"ru-en\", \"text\": [\"translation\"] }"
+  translation, _ := getTranslations([]byte(body))
+  if (translation.Texts[0] != "translation") {
+    t.Errorf("Translation was incorrect, got: %s, want: %s.", translation.Texts[0], "translation")
+  }
+}
+
+func TestFormatUnsplashSearhResult(t *testing.T) {
+  var searchJsonResult = "{}"
+  var imagesCollectionExpectedSize = 5
+
+  unsplashSearchResult := formatUnsplashSearchResult(searchJsonResult)
+
+  if(len(unsplashSearchResult) < imagesCollectionExpectedSize) {
+    t.Error("Images collection is incorrect, got %s, want %s.", len(unsplashSearchResult), imagesCollectionExpectedSize)
+  }
 }
