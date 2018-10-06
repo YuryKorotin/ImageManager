a38538099c812009445ce3bd78043aaa15d68b3b
YuryKorotin <yurykorotin73@gmail.com>
2018-10-06 23:09:54 +0400

    Move methods to worker

--- server/image_search_server.go
+++ server/image_search_server.go
@@ -46,7 +46,7 @@ func parseTranslationResponse(res *http.Response) string{
 func images(w http.ResponseWriter, r *http.Request) {
 	translationResultResponse := requestTranslationForQuery(r.URL.Query().Get("filter"))
 	translationResult := parseTranslationResponse(translationResultResponse)
-	imagesResult := requestImageFromUnsplash(translationResult)
+	imagesResult := RequestImageFromUnsplash(translationResult)
 	w.Write([]byte(imagesResult))
 }
 
@@ -75,35 +75,6 @@ func requestTranslationForQuery(query string) *http.Response {
 	return resp
 }
 
-func requestImageFromQwant(query string) string {
-	//var userAgentString = "Mozilla/5.0 (Windows NT 6.3; WOW64; rv:56.0) Gecko/20100101 Firefox/56.0"
-	offset := 0
-	locale := "en_en"
-	count := 1
-	var rootUrl = fmt.Sprintf(
-		"https://api.qwant.com/api/search/news/?count=%d&q=%s&offset=%d&locale=%s",
-		count,
-		query,
-		offset,
-		locale)
-	resp, _ := http.Get(rootUrl)
-
-	var bodyString = "NO RESULTS"
-
-	if resp.StatusCode == http.StatusOK {
-		bodyBytes, erro := ioutil.ReadAll(resp.Body)
-		bodyString = string(bodyBytes)
-		fmt.Println(erro)
-	}
-	for k, v := range resp.Header {
-		fmt.Printf("Headers, %s, %s \n", k, v)
-	}
-
-	fmt.Println(rootUrl)
-	fmt.Println(resp.StatusCode)
-	return bodyString
-}
-
 func FormatUnsplashSearchResult(searchResponseString string) UnsplashResponse{
   byteValue := []byte(searchResponseString)
 
@@ -123,28 +94,7 @@ func FormatGiphySearchResult(searchResponseString string) GiphyResponse {
   return response
 }
 
-func requestImageFromUnsplash(query string) string {
-	configuration := ApiKeysConfiguration{}
-	configPath, _ := filepath.Abs("image_manager/api_keys.json")
-	err := gonfig.GetConf(configPath, &configuration)
-	fmt.Println(err)
 
-	clientId := configuration.UnsplashApiKey
-	fmt.Println(clientId)
-	rootUrl := fmt.Sprintf("https://api.unsplash.com/search/photos?client_id=%s&query=%s", clientId, query)
-
-	resp, _ := http.Get(rootUrl)
-	var bodyString = "NO RESULTS"
-
-	if resp.StatusCode == http.StatusOK {
-		bodyBytes, erro := ioutil.ReadAll(resp.Body)
-		bodyString = string(bodyBytes)
-		fmt.Println(erro)
-	}
-	fmt.Println(rootUrl)
-	fmt.Println(resp.StatusCode)
-	return bodyString
-}
 
 func main() {
 	http.Handle("/", http.FileServer(http.Dir("./src")))
--- server/parallel_worker.go
+++ server/parallel_worker.go
@@ -3,8 +3,11 @@ package image_manager
 import (
 	"fmt"
 	"net/http"
-	"sort"
-	"time"
+	//"sort"
+	//"time"
+	"io/ioutil"
+	"github.com/tkanos/gonfig"
+	"path/filepath"
 )
 
 type ImageCollectionResult struct {
@@ -12,7 +15,60 @@ type ImageCollectionResult struct {
   UnsplashImageUrl []string
 }
 
+func RequestImageFromQwant(query string) string {
+	//var userAgentString = "Mozilla/5.0 (Windows NT 6.3; WOW64; rv:56.0) Gecko/20100101 Firefox/56.0"
+	offset := 0
+	locale := "en_en"
+	count := 1
+	var rootUrl = fmt.Sprintf(
+		"https://api.qwant.com/api/search/news/?count=%d&q=%s&offset=%d&locale=%s",
+		count,
+		query,
+		offset,
+		locale)
+	resp, _ := http.Get(rootUrl)
+
+	var bodyString = "NO RESULTS"
+
+	if resp.StatusCode == http.StatusOK {
+		bodyBytes, erro := ioutil.ReadAll(resp.Body)
+		bodyString = string(bodyBytes)
+		fmt.Println(erro)
+	}
+	for k, v := range resp.Header {
+		fmt.Printf("Headers, %s, %s \n", k, v)
+	}
+
+	fmt.Println(rootUrl)
+	fmt.Println(resp.StatusCode)
+	return bodyString
+}
+
+func RequestImageFromUnsplash(query string) string {
+	configuration := ApiKeysConfiguration{}
+	configPath, _ := filepath.Abs("image_manager/api_keys.json")
+	err := gonfig.GetConf(configPath, &configuration)
+	fmt.Println(err)
+
+	clientId := configuration.UnsplashApiKey
+	fmt.Println(clientId)
+	rootUrl := fmt.Sprintf("https://api.unsplash.com/search/photos?client_id=%s&query=%s", clientId, query)
+
+	resp, _ := http.Get(rootUrl)
+	var bodyString = "NO RESULTS"
+
+	if resp.StatusCode == http.StatusOK {
+		bodyBytes, erro := ioutil.ReadAll(resp.Body)
+		bodyString = string(bodyBytes)
+		fmt.Println(erro)
+	}
+	fmt.Println(rootUrl)
+	fmt.Println(resp.StatusCode)
+	return bodyString
+}
 
 func ParallelGet(concurrencyLimit int) ImageCollectionResult {
+	var result ImageCollectionResult
 
+	return result
 }
