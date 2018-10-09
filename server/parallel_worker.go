1d9ef4272eb5530eebfe8efb7e6bf081d94311ce
YuryKorotin <yurykorotin73@gmail.com>
2018-10-09 14:50:08 +0400

    And more reformatting

--- server/parallel_worker.go
+++ server/parallel_worker.go
@@ -1,13 +1,13 @@
 package image_manager
 
 import (
-	"fmt"
-	"net/http"
-	//"sort"
-	//"time"
-	"io/ioutil"
-	"github.com/tkanos/gonfig"
-	"path/filepath"
+  "fmt"
+  "net/http"
+  //"sort"
+  //"time"
+  "io/ioutil"
+  "github.com/tkanos/gonfig"
+  "path/filepath"
 )
 
 type GatheredCollectionResult struct {
@@ -21,32 +21,32 @@ type ImageCollectionResult struct {
 }
 
 func RequestImageFromQwant(query string) string {
-	//var userAgentString = "Mozilla/5.0 (Windows NT 6.3; WOW64; rv:56.0) Gecko/20100101 Firefox/56.0"
-	offset := 0
-	locale := "en_en"
-	count := 1
-	var rootUrl = fmt.Sprintf(
-		"https://api.qwant.com/api/search/news/?count=%d&q=%s&offset=%d&locale=%s",
-		count,
-		query,
-		offset,
-		locale)
-	resp, _ := http.Get(rootUrl)
+  //var userAgentString = "Mozilla/5.0 (Windows NT 6.3; WOW64; rv:56.0) Gecko/20100101 Firefox/56.0"
+  offset := 0
+  locale := "en_en"
+  count := 1
+  var rootUrl = fmt.Sprintf(
+	  "https://api.qwant.com/api/search/news/?count=%d&q=%s&offset=%d&locale=%s",
+	  count,
+	  query,
+	  offset,
+	  locale)
+  resp, _ := http.Get(rootUrl)
 
-	var bodyString = "NO RESULTS"
+  var bodyString = "NO RESULTS"
 
-	if resp.StatusCode == http.StatusOK {
-		bodyBytes, erro := ioutil.ReadAll(resp.Body)
-		bodyString = string(bodyBytes)
-		fmt.Println(erro)
-	}
-	for k, v := range resp.Header {
-		fmt.Printf("Headers, %s, %s \n", k, v)
-	}
+  if resp.StatusCode == http.StatusOK {
+    bodyBytes, erro := ioutil.ReadAll(resp.Body)
+    bodyString = string(bodyBytes)
+    fmt.Println(erro)
+  }
+  for k, v := range resp.Header {
+    fmt.Printf("Headers, %s, %s \n", k, v)
+  }
 
-	fmt.Println(rootUrl)
-	fmt.Println(resp.StatusCode)
-	return bodyString
+  fmt.Println(rootUrl)
+  fmt.Println(resp.StatusCode)
+  return bodyString
 }
 
 func RequestImageFromUnsplash(query string) string {
